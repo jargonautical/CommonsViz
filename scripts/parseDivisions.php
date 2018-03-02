10ec0c7d5e1bc04775dd319e4adbcf63e76e0ebc
Giuseppe Sollazzo <puntofisso@gmail.com>
2018-03-02 17:00:46 +0000

    Using new data sources

--- scripts/parseDivisions.php
+++ scripts/parseDivisions.php
@@ -1,65 +1,87 @@
 <?php
+# Originally:
 #http://hansard.services.digiminster.com/Divisions/list.json?startdate=2017-06-08
 #http://hansard.services.digiminster.com/Divisions/Division/102.json
 
-#TODO replace with
+
 # http://lda.data.parliament.uk/commonsdivisions.json?_pageSize=500&minEx-date=2017-06-08
-# http://api.data.parliament.uk/resources/files/800480.xml]
+# http://lda.data.parliament.uk/commonsdivisions/id/845870.json/xml less accurate->WHY?!
+# but can get info about member at: http://lda.data.parliament.uk/members/392.json
+# using this is easier: http://api.data.parliament.uk/resources/files/800480.xml
 
 # info at http://explore.data.parliament.uk/?learnmore=Commons%20Divisions
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,"http://hansard.services.digiminster.com/Divisions/list.json?startdate=2017-06-08");
+curl_setopt($ch, CURLOPT_URL,"http://lda.data.parliament.uk/commonsdivisions.json?_pageSize=25000&minEx-date=2017-06-08");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $text = curl_exec ($ch);
-
 curl_close ($ch);
-
 $json = json_decode($text);
+
 $out = array();
 
+foreach($json->result->items as $division) {
 
-// Instead of $json, use DOM, but still spit out JSON
-foreach($json->Results as $division) {
-  if ($division->House === "Commons") {
       $thisdiv = array();
-      $thisdiv['id'] = $division->Id;
-      $thisdiv['date'] = $division->Date;
-      $thisdiv['title'] = trim(preg_replace('/\s+/', ' ', $division->DebateSection));
-      $thisdiv['hansard'] = $division->HansardIdentifier;
+      $thisdiv['id'] = $division->uin;
+      $thisdiv['date'] = $division->date->_value;
+      $thisdiv['title'] = $division->title;
+      $thisdiv['url'] = $division->_about;
+      $thisdiv['hansard'] = $division->uin;
+      $thisdiv['preamble'] = "";
+      $thisdiv['PreVoteContent'] = "";
 
-      // Get full count
+      // Initialise arrays
+      $thisdiv['ayes'] = array();
+      $thisdiv['noes'] = array();
+      $thisdiv['ayestellers'] = array();
+      $thisdiv['noestellers'] = array();
+
+      // Build URL as it's not in the right format
+      $urlarr = explode("/",$division->_about);
+
+      $myurl = $urlarr[4];
+
+
+
+      // Get full data about this division
       $ch = curl_init();
       $divid = $division->Id;
-      curl_setopt($ch, CURLOPT_URL,"http://hansard.services.digiminster.com/Divisions/Division/$divid.json");
+      curl_setopt($ch, CURLOPT_URL,"http://lda.data.parliament.uk/commonsdivisions/id/$myurl.json");
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       $text = curl_exec ($ch);
       curl_close ($ch);
 
       $json_div = json_decode($text);
 
-      $thisdiv['preamble'] =$json_div[0]->Preamble;
-      $thisdiv['ayes'] = array();
-      $thisdiv['noes'] = array();
-      $thisdiv['ayestellers'] = array();
-      $thisdiv['noestellers'] = array();
-      // TODO <DivisionTitle>Nuclear Safeguards Bill: Report Stage Amdt 5</DivisionTitle>
 
-      $thisdiv['PreVoteContent'] = $json_div[0]->PreVoteContent;
-      // AyeCount
-      foreach ($json_div[0]->AyeMembers as $ayemember) {
-          $thisdiv['ayes'][] = "$ayemember->Id";
+
+      // Votes
+      foreach ($json_div->result->primaryTopic->vote as $ayemember) {
+          $idarr = $ayemember->member[0]->_about;
+          $mid = explode("/",$idarr)[4];
+          $type = $ayemember->type;
+
+          if ($type == "http://data.parliament.uk/schema/parl#AyeVote") {
+            // AYE
+            $thisdiv['ayes'][] = $mid;
+          } elseif ($type == "http://data.parliament.uk/schema/parl#NoVote") {
+            $thisdiv['noes'][] = $mid;
+          } else {
+            echo "ERROR IN VOTE TYPE ".$type;
+          }
       }
-      // NoeCount
-      foreach ($json_div[0]->NoeMembers as $noemember) {
-          $thisdiv['noes'][] = "$noemember->Id";
-      }
-      // TODO tellers (where will colour be?)
+
+
+      // TODO need to add info about party to display colour
+      // TODO replace use of csv file
+      // AyeTellers
+      // NoeTellers
 
 
       $out[] = $thisdiv;
 
-    }
+
   }
 
 echo json_encode($out);
